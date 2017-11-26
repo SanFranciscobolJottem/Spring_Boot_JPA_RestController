5983f2b6b1df7604657b2353470e49460353924e
Krisz <sanfranciscoboljottem@gmail.com>
2017-11-26 14:25:54 +0000

    nativeQuery

--- src/main/java/com/elsoproject/repository/StoryRepository.java
+++ src/main/java/com/elsoproject/repository/StoryRepository.java
@@ -2,7 +2,9 @@ package com.elsoproject.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.elsoproject.domain.Story;
@@ -16,7 +18,10 @@ public interface StoryRepository extends CrudRepository<Story, Long> {
 	//SELECT * FROM STORY WHERE posted IN (SELECT max(posted) FROM story) LIMIT 1;
 	Story findFirstByOrderByPostedDesc();
 
-	Story findByTitle(String title);
+//	@Query(value = "select * from story where title = ?1 limit 1", nativeQuery = true)
+//	@Query(value = "select * from story where title = :title limit 1", nativeQuery = true)
+	@Query(value = "select s from Story s where s.title = :title")
+	Story findByTitle(@Param("title") String title);
 	
 	List<Story> findAllByBloggerNameIgnoreCaseOrderByPostedDesc(String name);
 	
